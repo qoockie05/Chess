6e47dca7520590e9e94c03e0d2075d39d5c0e2c0
qoockie05 <qoockie05@gmail.com>
2026-04-14 10:49:42 +0200

    fixed lines.py

--- lines.py
+++ lines.py
@@ -1,8 +1,6 @@
-import torch
-import torchvision
 import cv2
 import numpy as np
-import torch
+
 img = cv2.imread('image1.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
